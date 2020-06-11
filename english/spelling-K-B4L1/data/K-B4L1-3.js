854f0fc7cdba901a53a995869b5cbc962afcdbd3
glglace <44548454+glglace@users.noreply.github.com>
2020-06-11 18:23:48 +0800

    第一次測試

---
+++ english/spelling-K-B4L1/data/K-B4L1-3.js
@@ -0,0 +1,4 @@
+﻿var URIformatVars = function(){/*--這一行請勿更改--
+&timer=600&
+line=3;whose,誰的;letter,信件;send,寄送;valentine,情人;strong,強大;feeling,感覺;rose,玫瑰;present,禮物;anyone,任何人;interest,興趣;spider,蜘蛛;dress up,打扮;as,如同;surprise,驚喜&
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
---
+++ english/spelling-K-B4L1/spelling_set.js
@@ -0,0 +1,13 @@
+﻿var URIformatVars = function(){/*--這一行請勿更改--
+&title=單字高手-生活作息篇&
+&number_enabled=no&
+&logger_url=&
+&datafolder=data&
+&menu=
+康軒國中英語 B4L1-一格,K-B4L1-1.js
+康軒國中英語 B4L1-兩格,K-B4L1-2.js
+康軒國中英語 B4L1-三格,K-B4L1-3.js
+康軒國中英語 B4L1-四格,K-B4L1-4.js
+康軒國中英語 B4L1-全部,K-B4L1-all.js
+&
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
